43b31b880807db71e8bf947a18b52b0bb47935d8
HSMell <hyde2598@gmail.com>
2022-09-27 00:23:47 +0900

    While, Break, Continue

--- StartPython2.py
+++ StartPython2.py
@@ -45,5 +45,41 @@ for x in my_tuple:
     print(x);
 
 person={'name' : 'KHS', 'age' : '29', 'weight' : ''};
-for x in person.values():
+for k, v in person.items():
+    print(x, v);
+
+fruit = 'apple';
+for x in fruit:
     print(x);
+
+
+# While
+max = 25;   #Maximum allowable weight
+weight = 0; #Current Weight
+item = 3;   #The weight of each package
+
+while weight + item <= max:
+    weight += item;
+    print('Add your luggage');
+print('My weight is {}'.format(weight));
+print('My weight is {weight}'.format(weight = weight));
+
+# Break;
+drama = ['season1','season2','season3','season4','season5'];
+for x in drama:
+    print('I watched {}'.format(x));
+    if x == 'season3':
+        print('{} is not so funny'.format(x));
+        break;
+print('Done with Breaking');
+print('------------------');
+
+# Continue
+drama = ['season1','season2','season3','season4','season5'];
+for x in drama:
+    if x == 'season3':
+        print('{} is not so funny'.format(x));
+        continue
+    print('{} watching'.format(x));
+print('Done with continue');
+print('------------------');
